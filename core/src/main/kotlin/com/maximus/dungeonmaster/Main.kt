b9e2fb3f7a07e3c8ba46a8e3dd198850fdc7b319
55_maximus_55 <lukinosh55@gmail.com>
2020-04-26 09:12:31 +0600

    Ktx i18n

--- core/src/main/kotlin/com/maximus/dungeonmaster/Main.kt
+++ core/src/main/kotlin/com/maximus/dungeonmaster/Main.kt
@@ -1,6 +1,8 @@
 package com.maximus.dungeonmaster
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Screen
+import com.badlogic.gdx.utils.I18NBundle
 import com.maximus.dungeonmaster.screens.GameScreen
 import ktx.app.KtxGame
 import ktx.inject.Context
@@ -11,6 +13,8 @@ class Main : KtxGame<Screen>() {
 
     override fun create() {
         context.register {
+            bindSingleton(I18NBundle.createBundle(Gdx.files.internal("i18n/lines")))
+
             bindSingleton(GameScreen())
         }
         addScreen(context.inject<GameScreen>())
