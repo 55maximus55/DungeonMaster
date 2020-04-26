f6da389121658a7fe79e6bd3d67d5fbb53472313
55_maximus_55 <lukinosh55@gmail.com>
2020-04-26 08:33:03 +0600

    Ktx inject

--- core/src/main/kotlin/com/maximus/dungeonmaster/Main.kt
+++ core/src/main/kotlin/com/maximus/dungeonmaster/Main.kt
@@ -3,13 +3,22 @@ package com.maximus.dungeonmaster
 import com.badlogic.gdx.Screen
 import com.maximus.dungeonmaster.screens.GameScreen
 import ktx.app.KtxGame
+import ktx.inject.Context
+
+val context = Context()
 
 class Main : KtxGame<Screen>() {
 
     override fun create() {
-        addScreen(GameScreen())
+        context.register {
+            bindSingleton(GameScreen())
+        }
+        addScreen(context.inject<GameScreen>())
 
         setScreen<GameScreen>()
     }
 
+    override fun dispose() {
+        context.dispose()
+    }
 }
