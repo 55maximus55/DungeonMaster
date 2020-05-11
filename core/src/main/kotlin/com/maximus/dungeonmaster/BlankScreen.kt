48c1b1dcb689503675b5037a5a7056153c1cd175
55_maximus_55 <lukinosh55@gmail.com>
2020-05-12 01:07:55 +0600

    ecs engine

--- core/src/main/kotlin/com/maximus/dungeonmaster/BlankScreen.kt
+++ core/src/main/kotlin/com/maximus/dungeonmaster/BlankScreen.kt
@@ -1,10 +1,16 @@
 package com.maximus.dungeonmaster
 
+import com.badlogic.ashley.core.PooledEngine
 import ktx.app.KtxScreen
 import ktx.vis.table
 
 open class BlankScreen : KtxScreen {
 
     open val view = table {}
+    val engine = PooledEngine()
+
+    override fun render(delta: Float) {
+        engine.update(delta)
+    }
 
 }
