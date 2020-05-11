ce855bb8e81dfff0096043e12c3c971b16e5c105
55_maximus_55 <lukinosh55@gmail.com>
2020-05-12 01:12:59 +0600

    GameScreen with map view

--- core/src/main/kotlin/com/maximus/dungeonmaster/screens/GameScreen.kt
+++ core/src/main/kotlin/com/maximus/dungeonmaster/screens/GameScreen.kt
@@ -1,8 +1,18 @@
 package com.maximus.dungeonmaster.screens
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.maps.tiled.TiledMap
 import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.math.Vector2
 import com.maximus.dungeonmaster.BlankScreen
+import com.maximus.dungeonmaster.box2d.systems.Box2dWorldDebugRenderSystem
+import com.maximus.dungeonmaster.box2d.systems.Box2dWorldUpdateSystem
+import com.maximus.dungeonmaster.box2d.utils.Box2dWallsCreator
+import com.maximus.dungeonmaster.tiledmap.OrthogonalTiledMapRenderSystem
+import ktx.ashley.add
+import ktx.box2d.createWorld
+import ktx.tiled.tileHeight
 import ktx.vis.table
 
 class GameScreen : BlankScreen() {
@@ -15,18 +25,37 @@ class GameScreen : BlankScreen() {
         label("Game Screen")
     }
 
+    val camera = OrthographicCamera().apply {
+        setToOrtho(false)
+        position.x = 0f
+        position.y = 0f
+        update()
+    }
+
     val map: TiledMap = TmxMapLoader().load("maps/map1.tmx")
+    val PPM = map.tileHeight.toFloat()
+    val world = createWorld(Vector2(0f, 0f))
+    val fov = 48f * 16
 
-    override fun show() {}
-    override fun hide() {}
+    init {
+        Box2dWallsCreator.createWalls(engine, map, world)
+        engine.add {
+            addSystem(Box2dWorldUpdateSystem(world))
+            addSystem(OrthogonalTiledMapRenderSystem(map, camera))
+            addSystem(Box2dWorldDebugRenderSystem(world, camera, PPM))
+        }
+    }
 
-    override fun render(delta: Float) {}
+    override fun resize(width: Int, height: Int) {
+        camera.apply {
+            viewportWidth = fov * Gdx.graphics.width.toFloat() / Gdx.graphics.height.toFloat()
+            viewportHeight = fov
+            update()
+        }
+    }
 
-    override fun pause() {}
-    override fun resume() {}
-
-    override fun resize(width: Int, height: Int) {}
-
-    override fun dispose() {}
+    override fun dispose() {
+        map.dispose()
+    }
 
 }
